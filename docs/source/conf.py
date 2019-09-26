2505c10e9b1e756bfc238ba4af042c350c26b819
Francesco Farina <farina.francesco1991@gmail.com>
2019-09-26 17:23:47 +0200

    mock mpi4py doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,18 @@
 #
 import os
 import sys
+
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['mpi4py']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 sys.path.insert(0, os.path.abspath('../..'))
 
 html_theme = "sphinx_rtd_theme"
