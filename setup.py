7737c9eadd256af7282f244b62814f3a50dd68f3
Francesco Farina <farina.francesco1991@gmail.com>
2019-09-27 21:13:56 +0200

    ff: patch 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="disropt",
-    version="0.1",
+    version="0.1.1",
     author="Francesco Farina, Andrea Camisa, Andrea Testa, Ivano Notarnicola, Giuseppe Notarstefano",
     author_email="franc.farina@unibo.it",
     description="DISROPT: a python framework for distributed optimization",
